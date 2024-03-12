208e47569259e2499cfd3a1b42a1440bf30fb166
dmy3ruk <alinka05alya@gmail.com>
2024-03-12 22:53:27 +0200

    config css/html

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('sign-up', views.sign_up, name='sign-up'),
     path('login', views.login_user, name='login'),
     path('logout', views.logout, name='logout'),
-    path('reset_password', auth_views.PasswordResetView.as_view(template_name="authentication/reset_password.html"), name='reset_password'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password', auth_views.PasswordResetView.as_view(template_name="authentication/forgot_password.html"), name='forgot_password'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="authentication/reset_password.html"), name='password_reset'),
     #path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
 
 ]
--- finances/settings.py
+++ finances/settings.py
@@ -128,6 +128,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATICFILES_DIRS=[
+    BASE_DIR / 'static'
+]
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
