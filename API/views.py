2fc27180202f44e948266cbdff310b56ec4f9be5
olesiadrv <115192323+olesiadrv@users.noreply.github.com>
2024-03-25 21:16:31 +0200

    api mono

--- API/views.py
+++ API/views.py
@@ -1,3 +1,65 @@
 from django.shortcuts import render
 
-# Create your views here.
+import monobank
+from datetime import datetime, date, timezone
+import time
+
+token = 'token'
+mono = monobank.Client(token)
+
+def get_cards():
+    try:
+        user_info = mono.get_client_info()
+        card_ids = [] 
+        if user_info:
+            for user in user_info['accounts']:
+                originBalance = user['balance'] // 100
+                temp = user['maskedPan'][0]
+                if temp[0] == '4':
+                    print('VISA ''Назва карти ', user['type'], 'Номер карти ', user['maskedPan'], 'Баланс по карті', originBalance)
+                elif temp[0] == '5':
+                    print('Master ''Назва карти ', user['type'], 'Номер карти ', user['maskedPan'], 'Баланс по карті', originBalance)
+                
+                card_ids.append(user['id']) 
+        return card_ids
+    except Exception as e:
+        print("Помилка у функції get_cards:", e)
+        return []
+
+def get_pay(user_ids):
+    originAmounts = [] 
+    try:
+        for user_id in user_ids: 
+            client = mono.get_statements(user_id, date(2024, 2, 23), date(2024, 2, 23))
+            for payment in client:
+                timeOrigin = datetime.fromtimestamp(payment['time'], timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
+                print('Час', timeOrigin)
+                originAmount = payment['amount'] // 100
+                print('Сума', originAmount)
+                originAmounts.append(originAmount) 
+                print('Категорія', payment['description'])
+                print('Валюта', payment['currencyCode'])
+                print('--------------------------------------')
+    except Exception as e:
+        print("Помилка у функції get_pay:", e)
+    return originAmounts 
+
+def get_category_earn_cost(originAmounts):
+    try:
+        for originAmount in originAmounts:
+            if str(originAmount).startswith('-'):
+                print("Витрата")
+            else:
+                print("Поповнення")
+    except Exception as e:
+        print("Помилка у функції get_category_earn_cost:", e)
+
+try:
+    card_ids = get_cards()
+    if card_ids:
+        time.sleep(61)
+        originAmounts = get_pay(card_ids) 
+        time.sleep(100)
+        get_category_earn_cost(originAmounts)  
+except Exception as e:
+    print("Помилка у головній програмі:", e)
