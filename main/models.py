e84dcc7ebb8badedc653c28fe8d82562ce9a1b1c
BilunykAnatolii <anbilunyk@gmail.com>
2024-05-16 23:10:48 +0300

    add currency to cards

--- main/models.py
+++ main/models.py
@@ -5,6 +5,7 @@ class Card(models.Model):
     id = models.AutoField(primary_key=True)
     card_id = models.CharField(max_length=100, unique=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    currency = models.CharField(max_length=10)
     balance = models.DecimalField(max_digits=10, decimal_places=2)
     card_number = models.CharField(max_length=16, unique=True)
     token = models.CharField(max_length=100, blank=True, null=True)
